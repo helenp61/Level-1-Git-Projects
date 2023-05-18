09ffe0a771e63802d835beac9f2929e7293cca0a
Helen Prasad <HPrasad@ormiston.school.nz>
2023-05-18 20:04:31 +1200

    Some changes

--- tkinter_checkbutton.py
+++ tkinter_checkbutton.py
@@ -6,9 +6,8 @@ from tkinter import ttk
 root = tk.Tk()
 my_boolean_var = tk.BooleanVar()
 
-my_checkbutton = ttk.Checkbutton(
-    text="Check when the option True",
-    variable=my_boolean_var
-)
+my_checkbutton = ttk.Checkbutton(text="Check when the option True",variable=my_boolean_var)
+
+
 my_checkbutton.pack()
 root.mainloop()
